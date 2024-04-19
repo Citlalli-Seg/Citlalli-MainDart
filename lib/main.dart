9f26ecc4d986032ee5d7bd3ad1fc17ceab3b7b22
Citlalli-Seg <citlaseg@gmail.com>
2024-04-19 14:44:22 -0600

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'app1',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 231, 185, 224)),
+          ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}class MyAppState extends ChangeNotifier {
+  var current = WordPair.random(); //Elegir una palabra de forma aleatoria
+  //Reasignará el elemento current con un nuevo WordPair aleatorio
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+ }
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>(); //Revisa el estado de la aplicacion
+    var pair = appState.current; 
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Una palabra grandiosa al azar:'),
+            BigCard(pair: pair), 
+            //Agregamos un boton 
+            ElevatedButton(
+              onPressed: () {
+                print('Presiona el boton!');
+                appState.getNext();  // ← This instead of print().
+              },
+              child: Text('Siguiente'),
+              ), 
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith( //tema de texto
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+         child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+} 
